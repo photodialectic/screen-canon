e5b2f0d743927cb4a3157b952b10bab6661747a4
Nick Hedberg <nick.hedberg@buzzfeed.com>
2015-06-29 16:50:07 -0500

    initial idea

---
+++ README.md
@@ -0,0 +1,4 @@
+clone dis repo
+`npm install`
+`node app.js`
+[http://localhost:3000/?url=buzzfeed.com&selector=.splash__container]
---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+var Pageres = require('pageres');
+var fs = require('fs');
+
+
+app.get('/', function (req, res) {
+	console.log(req);
+  var pageres = new Pageres({delay: 2})
+    .src(req.param('url'), ['iphone 5s'], {crop: true, selector: req.param('selector')})
+    .dest(__dirname);
+
+	pageres.run(function (err, items) {
+		res.sendFile(__dirname + '/' + items[0].filename);
+	});
+
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
