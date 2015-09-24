0099ac87b63faa419def39d09192e12f7e79be01
Nick Hedberg <nick.hedberg@buzzfeed.com>
2015-09-23 20:39:40 -0500

    add error handling for mongo server

--- model/db.js
+++ model/db.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose')
 	,	config   = require('config');
 
-mongoose.connect('mongodb://' + config.get('db_host') + '/' + config.get('db_name'));
+mongoose.connect('mongodb://' + config.get('db_host') + '/' + config.get('db_name'), function(err){
+	console.error('tried connecting to mongodb://%s/%s/', config.get('db_host'), config.get('db_name'));
+	console.error(err);
+});
